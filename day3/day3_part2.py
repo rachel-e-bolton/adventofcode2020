7ee4504afbfbe6bbb8122aaa1c9c10bf4303a15a
Rachel Bolton <50831008+rachel-e-bolton@users.noreply.github.com>
2020-12-08 15:38:20 +0200

    day3_part2

--- day3/day3_part2.py
+++ day3/day3_part2.py
@@ -1,16 +1,33 @@
 #!/usr/bin/python3
-index = 0
-tree_count = 0
+product = 0
 
-with open("input.txt", "r") as rows:
-  for row in rows:
-    max_index = len(row) - 1
-    if (row[index] == "#"):
-      tree_count += 1
+def slopeSpecificCount(across, down):
+  index = 0
+  tree_count = 0
+  row_count = 0
 
-    if (index + 3 <= max_index):
-      index += 3
-    else:
-      index = (index + 3 - max_index - 1)
+  with open("input.txt", "r") as rows:
+    for row in rows:
+      if ((row_count % down) == 0):
+        row = row.strip()
+        max_index = len(row) - 1
 
-print("Trees encountered: {}".format(tree_count))
+        if (row[index] == "#"):
+          tree_count += 1
+
+        if (index + across <= max_index):
+          index += across
+        else:
+          index = (index + across - max_index - 1)
+      row_count += 1
+    return tree_count
+
+print("Slope 1: {} trees".format(slopeSpecificCount(1,1)))
+print("Slope 2: {} trees".format(slopeSpecificCount(3,1)))
+print("Slope 3: {} trees".format(slopeSpecificCount(5,1)))
+print("Slope 4: {} trees".format(slopeSpecificCount(7,1)))
+print("Slope 5: {} trees".format(slopeSpecificCount(1,2)))
+
+product = (slopeSpecificCount(1,1)*slopeSpecificCount(3,1)*slopeSpecificCount(5,1)*slopeSpecificCount(7,1)*slopeSpecificCount(1,2))
+
+print("Product of trees encountered: {}".format(product))
